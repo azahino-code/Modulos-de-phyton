5799e18326357f47b41423f7ef793baf2ecca9fe
azahino- <azahino-@student.42urduliz.com>
2026-08-02 10:53:45 +0200

    module3/ex2finished

--- Python Module 03/ex2/ft_coordinate_system.py
+++ Python Module 03/ex2/ft_coordinate_system.py
@@ -12,20 +12,22 @@
 
 import math
 
+
 def get_player_pos(trys: int) -> tuple:
     try:
         text = input("Enter new coordinates as floats in format 'x,y,z':")
-        x, y, z = text.split(",")
-        x = float(x)
-        y = float(y)
-        z = float(z)
+        x: float
+        y: float
+        z: float
+        str_x, str_y, str_z = text.split(",")
+        x = float(str_x)
+        y = float(str_y)
+        z = float(str_z)
         coor = (x, y, z)
-        if len(coor) != 3:
-            raise ValueError("Invalid sintax")
         return coor
     except ValueError as error:
-        print(f"{error}")
-        return(get_player_pos(trys + 1))
+        print(f"ValueError: {error}")
+        return get_player_pos(trys + 1)
     finally:
         print(f"Number of trys: {trys}")
 
@@ -34,8 +36,9 @@ print("=== Game Coordinate System ===\n")
 print("Get a first set of coordinates")
 c = get_player_pos(1)
 print(f"It includes: X={round(c[0], 2)}, Y={round(c[1], 2)}, Z={round(c[2])}")
-res = math.sqrt(0 - c[0]**2 + (0 - c[1])**2 + (0 - c[2])**2)
+res = math.sqrt((0 - c[0])**2 + (0 - c[1])**2 + (0 - c[2])**2)
 print(f"Distance to center: {res}\n")
 print("Get a second set of coordinates")
 c2 = get_player_pos(1)
-res = math.sqrt(c2[0]-c[0]**2+(c2[1]-c[1])**2+(c2[2]-c[2])**2)
+res = math.sqrt((c2[0]-c[0])**2 + (c2[1]-c[1])**2 + (c2[2]-c[2])**2)
+print(f"Distance between the 2 sets of coordinates: {round(res, 4)}")
