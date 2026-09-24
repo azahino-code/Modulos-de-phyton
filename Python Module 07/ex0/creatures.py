b91ae0669f3c188e34d2e1872bbe969202e3976a
azahino- <azahino-@student.42urduliz.com>
2026-09-24 20:38:01 +0200

    Inicio de la batalla pokemon

---
+++ Python Module 07/ex0/creatures.py
@@ -0,0 +1,28 @@
+# *************************************************************************** #
+#                                                                             #
+#                                                         :::      ::::::::   #
+#    creatures.py                                       :+:      :+:    :+:   #
+#                                                     +:+ +:+         +:+     #
+#    By: azahino- <azahino-@student.42urduliz.com   +#+  +:+       +#+        #
+#                                                 +#+#+#+#+#+   +#+           #
+#    Created: 2026/09/24 20:31:13 by azahino-          #+#    #+#             #
+#    Updated: 2026/09/24 20:31:14 by azahino-         ###   ########.fr       #
+#                                                                             #
+# *************************************************************************** #
+
+from typing import ABC
+class Creature(ABC):
+    def __init__(self) -> None:
+        ...
+
+    def attack() -> str:
+        
+
+
+class Flameling(Creature):
+
+class Pyrodon(Creature):
+
+class Aquahub(Creature):
+
+class Torragon(Creature):
